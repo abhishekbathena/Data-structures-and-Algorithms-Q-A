c3b50052724d5b1a1a885e4bb87bee71a77f84b1
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2023-12-09 13:17:09 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (30.79%) - LeetHub

---
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<int>ans;
+        while(!q.empty())
+        {
+            int size=q.size();
+            while(size--)
+            {
+                TreeNode* top=q.front();
+                q.pop();
+                ans.push_back(top->val);
+                
+                if(top->left) q.push(top->left);
+                if(top->right) q.push(top->right);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        int res=ans[0];
+        for(int i=0;i<ans.size();i++)
+        {
+            if(ans[i]>res)
+            {
+                res=ans[i];
+                break;
+            }
+        }
+        if(res==ans[0]) return -1;
+        return res;
+    }
+};

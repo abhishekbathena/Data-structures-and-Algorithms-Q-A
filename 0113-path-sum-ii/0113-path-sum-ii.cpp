af5f9fd20273374b35a0ad0a7c0d5c2ce30841e9
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2023-12-17 19:11:20 +0530

    Time: 20 ms (12.32%), Space: 40.9 MB (6.48%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -10,41 +10,27 @@
  * };
  */
 class Solution {
-    vector<vector<int>>stk;
 public:
-        
-        void solve(TreeNode* root, int targetSum,int sum1,vector<int>trr){
-                if (root->left==NULL && root->right==NULL){
-                    if (sum1==targetSum){
-                        stk.push_back(trr);
-                    }
-                    return;
-                }
-                if(root->left){
-                    sum1+=root->left->val;
-                    trr.push_back(root->left->val);
-                    solve(root->left,targetSum,sum1,trr);
-                    sum1-=root->left->val;
-                    trr.pop_back();
-                }
-                if(root->right){
-                    sum1+=root->right->val;
-                    trr.push_back(root->right->val);
-                    solve(root->right,targetSum,sum1,trr);
-                    sum1-=root->right->val;
-                    trr.pop_back();
-                }
-
+    vector<vector<int>> path;
+    void hasPathSum(TreeNode* root, int targetSum, vector<int> vec) {
+        if(!root) return;
+        if(!root->right && !root->left) 
+        {
+            if(targetSum == root->val)
+            {
+                vec.push_back(root->val);
+                path.push_back(vec);
+            }
+            return;
         }
-
+        vector<int> ans = vec;
+        vec.push_back(root->val);
+      
+        hasPathSum(root->right, targetSum-root->val, vec) ;
+        hasPathSum(root->left, targetSum-root->val, vec);
+    }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        if (root==NULL){
-            return stk;
-        }
-        vector<int>trr;
-        int sum1=root->val;
-        trr.push_back(root->val);
-       solve(root,targetSum,sum1,trr);
-       return stk;
+        hasPathSum(root, targetSum, {});
+        return path;
     }
 };

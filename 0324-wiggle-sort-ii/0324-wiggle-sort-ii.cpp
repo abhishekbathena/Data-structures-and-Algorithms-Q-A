c54db0ca50c3a65ac24f899a14425e051724f967
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2023-12-18 11:58:16 +0530

    Time: 13 ms (75.08%), Space: 18.9 MB (10.40%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -2,21 +2,32 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         vector<int>first1,first2;
-        vector<int>temp(nums.begin(),nums.end());
-        sort(temp.begin(),temp.end(),greater<int>());
+        sort(nums.begin(),nums.end());
         int n=nums.size();
-        int j=0;
-        for(int i=1;i<n;i=i+2)
+        int mid;
+        if(n%2) mid=n/2;
+        else mid=(n/2)-1;
+        for(int i=0;i<n;i++)
         {
-            nums[i]=temp[j];
-            j++;
+            if(i<=mid) first1.push_back(nums[i]);
+            else first2.push_back(nums[i]);
         }
-        for(int i=0;i<n;i=i+2)
+        reverse(first2.begin(),first2.end());
+        reverse(first1.begin(),first1.end());
+        int f1=first1.size();
+        int f2=first2.size();
+        int l=0,m=0;
+        nums.clear();
+        while(l<f1 && m<f2)
         {
-            nums[i]=temp[j];
-            j++;
+            nums.push_back(first1[l]);
+            nums.push_back(first2[m]);
+            l++;
+            m++;
         }
-
+        if(l<f1) nums.push_back(first1[l]);
+        else if(m<f2) nums.push_back(first2[m]);
+        
     
     }
 };

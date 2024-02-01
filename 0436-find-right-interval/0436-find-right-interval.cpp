011ccc519af22fda287a1f8fa97c6eda304c553c
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2024-02-01 13:52:02 +0530

    Time: 59 ms (70.23%), Space: 31.8 MB (19.51%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -6,7 +6,6 @@ public:
         for(int i=0; i<n; i++)  temp.insert({intervals[i][0], i});
         vector<int> ans;
         for(int i=0; i<intervals.size(); i++){
-            int tgt = intervals[i][0];
             auto res = temp.lower_bound(intervals[i][1]);
             if(res != temp.end()) ans.push_back(res->second);
             else ans.push_back(-1);

14509437ce46bc4de62ea0ad5fac6b6f3b2c198b
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2024-03-04 11:50:59 +0530

    Time: 4 ms (74.87%), Space: 13.1 MB (23.53%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        sort(tokens.begin(), tokens.end());
-        int i = 0, j = tokens.size() - 1;
-        int maxi = 0;
-        int score = 0;
-        while (i <= j) { // Changed condition to handle the case when i and j meet
-            if (power >= tokens[i]) {
+        sort(tokens.begin(),tokens.end());
+        int i=0,j=tokens.size()-1;
+        int maxi=0;
+        int score=0;
+        while(i<=j)
+        {
+            if(power>=tokens[i])
+            {
                 score++;
-                power -= tokens[i];
-                maxi = max(maxi, score);
+                power=power-tokens[i];
+                maxi=max(maxi,score);
                 i++;
-            } else if (score > 0) { // Added condition to ensure we have at least 1 score to play tokens face-down
-                score--;
-                power += tokens[j];
-                maxi = max(maxi, score);
-                j--;
-            } else {
-                break; // If we cannot play any token, break the loop
             }
+            else if(score>0)
+            {
+                score--;
+                power=power+tokens[j];
+                maxi=max(maxi,score);
+                j--;
+            }
+            else
+            {
+                break;
+            }
+            
         }
+        
         return maxi;
     }
 };

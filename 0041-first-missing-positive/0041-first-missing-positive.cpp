ada40feda1e9754c974d653f6d7387d54f70fc49
Abhishek <66793199+abhishekbathena@users.noreply.github.com>
2024-03-26 21:06:35 +0530

    Time: 43 ms (62.28%), Space: 43.7 MB (55.21%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+class Solution {
+public:
+    int firstMissingPositive(std::vector<int>& nums) {
+        int n = nums.size();
+        
+        // Rearrange the array so that nums[i] = i + 1 for all i in [0, n)
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                std::swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        
+        // Find the first positive integer that is not present
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        // If all positive integers from 1 to n are present, return n + 1
+        return n + 1;
+    }
+};
